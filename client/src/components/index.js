f4bc5d38970f2743023dc44a2e881fcb14d4618d
SACRED <>
2022-05-15 21:28:58 +0300

    15.05.2022

---
+++ client/src/components/index.js
@@ -0,0 +1,3 @@
+import Drawer from "./drawer";
+
+export { Drawer };
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import Dashboard from "./dashboard";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <Dashboard />
+  </React.StrictMode>
+);
